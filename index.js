15e3f5ea2a97ca7845ca01370d1b66042cdc2eec
Azamat <73574661+azadov-azamat@users.noreply.github.com>
2024-09-09 17:02:52 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,12 @@ const axios = require('axios');
 const FormData = require('form-data');
 
 async function getSMSProviderToken() {
-    // const KEY = redisKey('provider-token');
     let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTc3NTY3MDAsImlhdCI6MTcxNTE2NDcwMCwicm9sZSI6InVzZXIiLCJzaWduIjoiNjM0YmY5ZTM0ZDBkN2ZkMTMzMjg5YzQ1ZmY0ZDA0M2I4MzBhYWRkNmQ3YzYyYWQ0YmI4OWMxNmJiNzMwOGYwOSIsInN1YiI6IjQ5NTEifQ.0HMYNx11FOaLbxh9ndGevz-6AXh0xKGrcoN2nO9W4cg';
 
     if (token) {
         return token;
     }
-    console.log(token)
+ 
     let form = new FormData();
     form.append('email', process.env.ESKIZ_UZ_EMAIL);
     form.append('password', process.env.ESKIZ_UZ_PASSWORD);
@@ -26,25 +25,17 @@ async function getSMSProviderToken() {
 
     token = data.data?.token;
 
-    // if (token) {
-    //     await redisClient.set(KEY, token, 'EX', (DAY * 30) / 1000);
-    // }
-
     return token;
 }
 
 async function sendMessage(phone, body) {
-    // if (isTest) {
-    //     return;
-    // }
-
     let token = await getSMSProviderToken();
 
     let form = new FormData();
     form.append('mobile_phone', `998${phone}`);
     form.append('message', body);
     form.append('from', '4546');
-    // form.append('callback_url', 'http://0000.uz/test.php');
+  
     try {
         let {data} = await axios.post("https://notify.eskiz.uz/api/message/sms/send", form, {
             headers: {
@@ -62,13 +53,7 @@ async function sendMessage(phone, body) {
 function sendSms() {
     const message = "Уважаемые риелторы! Представляем уникальный проект 77kv.uz - первое приложение интеграции телеграмм для удобной работы недвижимостью. Регистрируйтесь сегодня и получите сайт бесплатно. По вопросам и предложениям: +998200027700 https://t.me/marina_laty";
 
-    const numbers = [
-        "911717818",
-        "909606896",
-        "948928811",
-        "941853560",
-        "888383003"
-    ]
+    const numbers = []
 
     for (const number of numbers) {
         sendMessage(number, message)
